65eb5928c3a6f465dc84b9a2e90287893ee263c1
ramazanov.null <ramazanov.null@gmail.com>
2019-10-30 06:11:05 +0300

    complit, уцы
    камплитуцы, ну или почти камплит

--- perceptron.go
+++ perceptron.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"fmt"
+	"time"
 	"strconv"
 	"math/rand"
 	"encoding/csv"
@@ -14,10 +15,10 @@ var numbers = []string{
 	"five", "six", "seven", "eight", "nine"}
 
 var endElementDigitInStringSlice = 900
-var numberOfIterations = 1000000;
+var numberOfIterations = 999999999;
 var currentNumber int;
 var numberOfPixel = 784;
-var threshold = 300;
+var threshold = 1;
 var weights [784]int;
 var digitInStringSlice [][]string
 
@@ -36,59 +37,78 @@ func education() {
 	var digit int
 	var index int
 	var flag bool
+	fmt.Println("currentNumber = " + strconv.Itoa(currentNumber))
 	for i := 0; i < numberOfIterations; i++ {
 		digit = rand.Intn(10)
 		index = rand.Intn(endElementDigitInStringSlice)
-		flag = proceed(digit, index)
+		flag = discrimination(digit, index)
 		if digit == currentNumber {
 			if (!flag) {
-				editweights(digitInStringSlice[digit][index], -1)
+				editweights(digitInStringSlice[digit][index], 1)
 			}
 		} else {
 			if (flag) {
-				editweights(digitInStringSlice[digit][index], 1)
+				editweights(digitInStringSlice[digit][index], -1)
 			}
 		}
 	}
 	return
 }
 
-func proceed(digit int, index int) bool {
+var o int
+
+func discrimination(digit int, index int) bool {
 	sum := 0
 	for i := 0; i < numberOfPixel; i++{
 		if digitInStringSlice[digit][index][i] == '1'{
 			sum += weights[i]
 		}
 	}
+	// fmt.Println(sum)
+	// if o > 5 {
+	// 	os.Exit(0)
+	// }
+	// o++
 	return sum >= threshold
 }
 
 func editweights(digit string, cof int)  {
 	for i := 0; i < numberOfPixel; i++ {
-		if(int(digit[i]) == 1) {
-			weights[i] +=  cof
+		if(digit[i] == '1') {
+			weights[i] += cof
 		}
 	}
 }
 
 func main() {
-	currentNumber, _ := strconv.Atoi(os.Args[1])
+	rand.Seed(time.Now().UnixNano())
 	digitInStringSlice = make([][]string, 10)
 	for i := 0; i < 10; i++ {
 		digitInStringSlice[i] = getDigitInStringSlice("CSVs" + string(os.PathSeparator) + strconv.Itoa(i) + ".csv")
 	}
+	currentNumber, _ = strconv.Atoi(os.Args[1])
 	education()
 
 	index := 0
 	for i := 0; i < 10; i++ {
 		index = rand.Intn(100) + 800
-		if proceed(currentNumber, index) {
-			fmt.Println("YES")
+		if discrimination(currentNumber, index) {
+			fmt.Print("YES\t")
 		} else {
-			fmt.Println("NO")
+			fmt.Print("NO\t")
 		}
 	}
-	for index := 0; index < 784; index++ {
-		fmt.Println(weights[index])
-	}
+
+	// fmt.Println()
+	// for index := 0; index < 784; index++ {
+	// 	if weights[index] <= 0{
+	// 		fmt.Println(weights[index])
+	// 	}
+	// }
+	// fmt.Println()
+	// for index := 0; index < 784; index++ {
+	// 	if weights[index] > 0{
+	// 		fmt.Println(weights[index])
+	// 	}
+	// }
 }
